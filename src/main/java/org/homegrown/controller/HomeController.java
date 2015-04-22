cce589ae312a0eb6a5272e8fb17b08eea22a91b7
JLEFE1 <jorislefever@hotmail.com>
2015-04-22 16:03:38 +0200

    Change to Apache tiles
    Adding properties, dependencies and tiles

--- src/main/java/org/homegrown/controller/HomeController.java
+++ src/main/java/org/homegrown/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
         model.addAttribute("message", "Hello world!");
-        return "hello";
+        return "home";
     }
 
 }
