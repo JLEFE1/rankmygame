1dfed18015297fef03d5b4ed78ffb8151367ab43
JLEFE1 <jorislefever@hotmail.com>
2015-04-27 11:53:27 +0200

    - Refactoring code - adding editing pages - ...

--- src/main/java/org/homegrown/controller/HomeController.java
+++ src/main/java/org/homegrown/controller/HomeController.java
@@ -14,9 +14,17 @@ public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
-        model.addAttribute("message", "Hello world!");
-
         return "home";
+    }
+
+    @RequestMapping(value = "disclaimer", method = RequestMethod.GET)
+    public String disclaimer(ModelMap model) {
+        return "disclaimer";
+    }
+
+    @RequestMapping(value = "contact", method = RequestMethod.GET)
+    public String contact(ModelMap model) {
+        return "contact";
     }
 
 }
--- src/main/java/org/homegrown/controller/PlayerController.java
+++ src/main/java/org/homegrown/controller/PlayerController.java
@@ -1,9 +1,19 @@
 package org.homegrown.controller;
 
+import org.homegrown.domain.Player;
+import org.homegrown.service.PlayerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by JoLe on 22/04/15.
@@ -12,10 +22,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/players")
 public class PlayerController {
 
+    final Logger logger = LoggerFactory.getLogger(PlayerController.class);
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Autowired
+    private PlayerService playerService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String show(@PathVariable("id") Long id, Model uiModel){
+        uiModel.addAttribute("player", playerService.findById(id));
+        return "players/show";
+    }
+
+    @RequestMapping(value = "/{id}", params="form", method = RequestMethod.GET)
+    public String updateForm(@PathVariable("id") Long id, Model uiModel){
+        uiModel.addAttribute("player", playerService.findById(id));
+        return "players/updateForm";
+    }
+
     @RequestMapping(params = "form", method = RequestMethod.GET)
-    public String newPlayer(ModelMap model) {
-        model.addAttribute("message", "Hello world!");
-        return "players";
+    public String getPlayerList(ModelMap uiModel) {
+
+        logger.info("Listing players");
+
+        List<Player> players = playerService.findAll();
+        uiModel.addAttribute("players", players);
+
+        logger.info("No. of players: " + players.size());
+
+        return "players/playerlist";
     }
 
 }
