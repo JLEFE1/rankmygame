896149c4de9eed09c74831cb66a252399b9b263c
JLEFE1 <jorislefever@hotmail.com>
2015-05-11 15:06:05 +0200

    Adding extra jQuery libs

--- src/main/webapp/scripts/lastGames.js
+++ src/main/webapp/scripts/lastGames.js
@@ -1,4 +1,20 @@
 /**
  * Created by JoLe on 08/05/15.
  */
-$(document).ready(function(){});
+$(document).ready(function () {
+    $(".resultName").on("mouseenter", function () {
+        $(this).closest("#playerStats").find(".result").show();
+        $(this).addClass('highlighted');
+    });
+    $(".resultName").on("mouseleave", function () {
+        $(this).closest("#playerStats").find(".result").hide();
+        $(this).removeClass('highlighted');
+    });
+
+    $(".game").on("mouseenter", function () {
+        $(".winner").show();
+    });
+    $(".game").on("mouseleave", function () {
+        $(".winner").hide();
+    });
+});
